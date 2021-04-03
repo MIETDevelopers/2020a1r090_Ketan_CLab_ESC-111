71c49af19fec327c127c7c76ef4232fca918d1e6
=Ketan <=2020a1r090@mietjammu.in>
2021-04-03 13:30:44 +0530

    Program to calculate factorial of a number using a for loop

---
+++ lab10.c
@@ -0,0 +1,17 @@
+#include<stdio.h> //Header File Input Output
+#include<math.h>//header file mathematical functions
+int main()//main function
+{
+	int num, count, fact = 1;
+
+	printf("Enter a number to find its Factorial\n");
+	scanf("%d", &num);//reading input
+
+	for(count = 1; count <= num; count++)//for loop
+	{
+	   fact = fact * count;
+	}//num=5, factorial=120
+	printf("Factorial of %d is %d\n", num, fact);//output
+
+	return 0;
+}
