08b528d6ec9985d74f4ca830191029c4f169da27
=Ketan <=2020a1r090@mietjammu.in>
2021-04-12 19:37:47 +0530

    Write a C program to perform 2-dimensional matrix multiplication passing the two dynamic sized matrices to functions

---
+++ lab33.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void mul(int [3][3] , int [3][3]);
+int main(){
+    int a[3][3],b[3][3],i,j;
+    printf("Enter First Matrix\n");
+    for(i=0;i<3;i++)
+        for(j=0;j<3;j++)
+            scanf("%d", &a[i][j]);
+    printf("Enter second Matrix\n");
+    for(i=0;i<3;i++)
+        for(j=0;j<3;j++)
+            scanf("%d", &b[i][j]); 
+    mul(a,b);     //function calling
+    return 0;       
+}
+void mul(int a[3][3] , int b[3][3])
+{
+    int c[3][3],i,j,k;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            c[i][j]=0;
+            for(k=0;k<3;k++){
+                c[i][j] = c[i][j] + a[i][k] *b[k][j];
+            }
+        }
+    }
+    printf("result is....\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++){
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+}
